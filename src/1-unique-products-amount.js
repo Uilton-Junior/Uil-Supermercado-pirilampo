238b6e1f84b12b1dddf68c0b120a966fb5f98643
Uilton Junior <uilton.junior@hotmail.com.br>
2024-04-29 00:34:34 -0300

    crio uma let amout que recebe 0

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -2,6 +2,15 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+  
+  // crio uma let amount
+  let amount = 0 
+
+  
+
+
+
 };
+
 
 module.exports = { getUniqueProductsAmount };
