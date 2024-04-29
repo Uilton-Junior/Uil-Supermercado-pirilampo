826c6f7ac2cf5fb30dd7c3f4ce33ad0876cb4fc1
Uilton Junior <uilton.junior@hotmail.com.br>
2024-04-29 00:46:44 -0300

    adiciono um console.log para checar se o codigo esta funcionando

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -3,22 +3,16 @@ const stockProducts = require('./data.json');
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
   
-  // crio uma let amount
-  let amount = 0 
-
+  let amount = 0   // crio uma let amount
   // crio um loop for para percorrer todo o stockProducts
-
   for(let index = 0 ; index < stockProducts.length ; index++){
 
     // incrementa o contador a cada interação
     amount++
-
   }
   return amount
 
 };
 
-
-
-
+console.log(getUniqueProductsAmount())
 module.exports = { getUniqueProductsAmount };
