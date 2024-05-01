234bbbc815106549f5a121317f3c9070fbb26e99
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-01 00:11:22 -0300

    crio um loop for para percorrer todo o stockProducts

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -6,6 +6,13 @@ const getUniqueProductsName = () => {
   // crio uma const que recebe uma array vazia para que  no final os produtos sejam armazenados nela
 
   let productList = []
+   // crio um loop for para percorrer todo o stockProducts
+ for( let index = 0 ; index < stockProducts.length ; index+=1){
+  let products = stockProducts[index]
+
+   
+}
+return productList
 
 };
 
