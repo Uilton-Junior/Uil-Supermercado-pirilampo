7b674f2dc8383f073c04b2b551e70b0708908987
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-01 00:14:17 -0300

    crio uma condição para que lançe todas as strings com a chave productName dentro da array productlist

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -9,6 +9,14 @@ const getUniqueProductsName = () => {
    // crio um loop for para percorrer todo o stockProducts
  for( let index = 0 ; index < stockProducts.length ; index+=1){
   let products = stockProducts[index]
+   // crio uma condição para que lançar todas as strings com a chave productName dentro da array productlist onde:
+
+    // se houver uma chave productName em products e os valores dessa chave for do tipo string...
+    if( 'productName' in products && typeof products.productName === 'string'){
+      
+      // lance  todos os productName dentro da minha array  productList
+      productList.push(products.productName)
+    }
 
    
 }
