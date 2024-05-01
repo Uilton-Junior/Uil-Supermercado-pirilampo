6536ba2341d00b9968c46d28b5765d21eaad82a9
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-01 00:02:11 -0300

    deixo o console.log em forma de comentario

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -2,6 +2,12 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
+
+  // crio uma const que recebe uma array vazia para que  no final os produtos sejam armazenados nela
+
+  let productList = []
+
+ 
 };
 
 module.exports = { getUniqueProductsName };
