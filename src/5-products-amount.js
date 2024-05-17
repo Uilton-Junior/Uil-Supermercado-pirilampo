262b0d6c881948203188dd0ec8e5ebe9dc5060fb
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-17 01:42:33 -0300

    crio um loop para percorrer todo o estoque e uma let product que recebe o resultado do loop

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -3,6 +3,10 @@ const stockProducts = require('./data.json');
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
   let soma = 0
+  for(let index = 0 ; index < stockProducts.length ; index+=1){
+  
+    let product = stockProducts[index]
+  }
  
 };
 console.log(getProductsAmount())
