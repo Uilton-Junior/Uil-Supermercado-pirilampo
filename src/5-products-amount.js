2b8e898c90534137bc60f06b049331c782b80ebf
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-17 01:40:53 -0300

    crio uma let soma que recebe o number 0

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -2,7 +2,7 @@ const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
-  
+  let soma = 0
  
 };
 console.log(getProductsAmount())
