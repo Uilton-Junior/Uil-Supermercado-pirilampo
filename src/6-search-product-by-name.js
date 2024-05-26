468149e53b92e4b317b0db7930b08c3a8248028a
Uilton Junior <uilton.junior@hotmail.com.br>
2024-05-26 14:21:15 -0300

    aplico um return para retornar um objeto , crioa propriedade description que recebe a descrição do produto

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -6,6 +6,17 @@ const searchProductByName = (search) => {
 
  if (!search) return null;
  
+  // método find() : Essa função verifica se o nome do produto (convertido para minúsculas) é igual ao nome do produto a ser buscado (também convertido para minúsculas).
+ const produto = stockProducts.find(produto => produto.productName.toLowerCase() === search.toLowerCase());
+
+if(!produto) return null;
+
+return {
+
+  description: produto.description,
+  
+
+}
 
 
 };
